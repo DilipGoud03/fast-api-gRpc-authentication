437a3376cb7a0f52c87032adcd7599ba9662ae40
Dilip-Goud-03 <dilipgaurh2017@gmail.com>
2025-09-13 17:53:36 +0530

    Update sh

--- proto-generate.sh
+++ proto-generate.sh
@@ -1,9 +1,5 @@
 #!bin/bash
 # This file is used to generate the proto files for a module bith given paths
-# eg: sh proto-generate.sh ./communication-api-service/proto/communication_proto/meter_text/meter_text.proto ./communication-api-service/proto/communication_proto ./communication-api-service/proto/communication_proto
-# eg: sh proto-generate.sh ./invoice-api-service/proto/invoice_proto/invoices/invoice.proto ./invoice-api-service/proto/invoice_proto ./invoice-api-service/proto/invoice_proto
-# eg: sh proto-generate.sh ./payment-api-service/proto/payments_proto/transactions/transactions.proto ./payment-api-service/proto/payments_proto ./payment-api-service/proto/payments_proto
-# eg: sh proto-generate.sh ./survey-api-service/proto/survey_proto/survey/survey.proto ./survey-api-service/proto/survey_proto ./survey-api-service/proto/survey_proto
 # eg: sh proto-generate.sh ./user-api-service/proto/user_proto/user_user/user_user.proto ./user-api-service/proto/user_proto ./user-api-service/proto/user_proto
 
 DEFAULT_DIR="."
